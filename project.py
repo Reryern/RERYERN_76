9b4bba583242c037cfe0ddbcb8923aefed34db12
Reryern <kercymreryern@gmail.com>
2025-04-10 13:23:21 +0300

    update

--- project.py
+++ project.py
@@ -1,22 +1,22 @@
 inventory = [
     {"name": "pens", "qty": 100, "unit_price": 1000, "cost_price": 800, "product number": "A01", "brand": "nataraj"},
-    {"name": "pens", "qty": 100, "unit_price": 700, "cost_price": 500, "product number": "A02", "brand": "bic"},
-    {"name": "pens", "qty": 100, "unit_price": 500, "cost_price": 400, "product number": "A03", "brand": "nice"},
-    {"name": "books", "qty": 100, "unit_price": 1500, "cost_price": 1200, "product number": "B01", "brand": "vista"},
-    {"name": "books", "qty": 100, "unit_price": 2000, "cost_price": 1700, "product number": "B02", "brand": "araqay"},
-    {"name": "books", "qty": 100, "unit_price": 1000, "cost_price": 800, "product number": "B03", "brand": "classic"},
-    {"name": "pencils", "qty": 100, "unit_price": 900, "cost_price": 700, "product number": "C01", "brand": "nataraj"},
-    {"name": "pencils", "qty": 100, "unit_price": 700, "cost_price": 500, "product number": "C02", "brand": "bic"},
-    {"name": "pencils", "qty": 100, "unit_price": 900, "cost_price": 750, "product number": "C03", "brand": "nice"},
-    {"name": "sets", "qty": 100, "unit_price": 9000, "cost_price": 8000, "product number": "D01", "brand": "oxford"},
-    {"name": "sets", "qty": 100, "unit_price": 7000, "cost_price": 6000, "product number": "D02", "brand": "nataraj"},
-    {"name": "sets", "qty": 100, "unit_price": 5000, "cost_price": 4000, "product number": "D03", "brand": "picfare"},
-    {"name": "rubbers", "qty": 120, "unit_price": 700, "cost_price": 500, "product number": "EO1", "brand": "nataraj"},
-    {"name": "rubbers", "qty": 120, "unit_price": 500, "cost_price": 300, "product number": "EO2", "brand": "bic"},
-    {"name": "rubbers", "qty": 120, "unit_price": 200, "cost_price": 100, "product number": "EO3", "brand": "nice"},
+    {"name": "pens", "qty": 100, "unit_price": 700, "cost_price": 500, "product number": "A01", "brand": "bic"},
+    {"name": "pens", "qty": 100, "unit_price": 500, "cost_price": 400, "product number": "A01", "brand": "nice"},
+    {"name": "books", "qty": 100, "unit_price": 1500, "cost_price": 1200, "product number": "A02", "brand": "vista"},
+    {"name": "books", "qty": 100, "unit_price": 2000, "cost_price": 1700, "product number": "A02", "brand": "araqay"},
+    {"name": "books", "qty": 100, "unit_price": 1000, "cost_price": 800, "product number": "A02", "brand": "classic"},
+    {"name": "pencils", "qty": 100, "unit_price": 900, "cost_price": 700, "product number": "A03", "brand": "nataraj"},
+    {"name": "pencils", "qty": 100, "unit_price": 700, "cost_price": 500, "product number": "A03", "brand": "bic"},
+    {"name": "pencils", "qty": 100, "unit_price": 900, "cost_price": 750, "product number": "A03", "brand": "nice"},
+    {"name": "sets", "qty": 100, "unit_price": 9000, "cost_price": 8000, "product number": "A04", "brand": "oxford"},
+    {"name": "sets", "qty": 100, "unit_price": 7000, "cost_price": 6000, "product number": "A04", "brand": "nataraj"},
+    {"name": "sets", "qty": 100, "unit_price": 5000, "cost_price": 4000, "product number": "A04", "brand": "picfare"},
+    {"name": "rubbers", "qty": 120, "unit_price": 700, "cost_price": 500, "product number": "A05", "brand": "nataraj"},
+    {"name": "rubbers", "qty": 120, "unit_price": 500, "cost_price": 300, "product number": "A05", "brand": "bic"},
+    {"name": "rubbers", "qty": 120, "unit_price": 200, "cost_price": 100, "product number": "A05", "brand": "nice"},
 ]
 
-def add_item():
+'''def add_item():
     name = input("Enter item name: ")
     quantity = int(input("Enter the quantity: "))
     price = int(input("Enter the price: "))
@@ -34,8 +34,17 @@ def add_item():
         "product number":product_number
     }
     inventory.append(item)
-    print(item)
+    print(f"The added item is{item}")
+    print(inventory)
 
     return item
-add_item()
+add_item()'''
 
+def search_item(name):
+    search_item = input("Enter the item you want: ")
+    for item in inventory:
+        if search_item == item["name"]:
+            print(f"name: {name}")
+    return search_item
+search_item(search_item)
+    
